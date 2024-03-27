16dd8fe8251899a75e30f7a2a16b487b4461f387
danghdat <danghdat@hanatourjsv.com>
2024-03-27 09:21:27 +0700

    improve longest string using 2 pointer

--- code/TheLongestString.java
+++ code/TheLongestString.java
@@ -1,5 +1,7 @@
 package code;
 
+import java.util.HashMap;
+
 public class TheLongestString {
     public static void main(String[] argv) {
         System.out.println(lengthOfLongestSubstring("anviaj"));
@@ -35,13 +37,34 @@ public class TheLongestString {
             if (!addValue1) {
                 value2.append(array[i]);
             }
-            if(result<value1.length()){
+            if (result < value1.length()) {
                 result = value1.length();
             }
-            if(result<value2.length()){
+            if (result < value2.length()) {
                 result = value2.length();
             }
         }
         return result;
+    }
+
+    public static int lengthOfLongestSubstringImprove(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        int maxLength = 0;
+        int start = 0;
+
+        for (int end = 0; end < s.length(); end++) {
+            char currentChar = s.charAt(end);
+            if (map.containsKey(currentChar)) {
+                start = Math.max(start, map.get(currentChar) + 1);
+            }
+            map.put(currentChar, end);
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+
+        return maxLength;
     }
 }
